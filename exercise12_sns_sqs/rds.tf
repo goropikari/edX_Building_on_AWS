e5b5ddac1741af8fda07c2aa54668a5db01a0aa3
goropikari <goropikari56@gmail.com>
2019-10-19 13:53:01 +0900

    skip_final_snapshot=true for terraform destroy

--- exercise12_sns_sqs/rds.tf
+++ exercise12_sns_sqs/rds.tf
@@ -11,6 +11,7 @@ resource "aws_db_instance" "default" {
   parameter_group_name   = "default.mysql5.7"
   vpc_security_group_ids = [aws_security_group.mysql_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+  skip_final_snapshot    = true # for terraform destroy
 
   lifecycle {
     ignore_changes = [password]
